5f5df6883bf166b1a1ed049f361790567d9ecd12
zmalshara <zmalshara@10.242.237.100>
2018-12-13 14:04:00 +0200

    First Time

---
+++ codeanalysis/jo.edu.just.se.jdt.codeanalysis/src/jo/edu/just/se/jdt/codeanalysis/model/MethodInformation.java
@@ -0,0 +1,54 @@
+package jo.edu.just.se.jdt.codeanalysis.model;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IMethod;
+
+/**
+* 
+* @author Zakarea AL SHARA
+*
+*/ 
+
+public class MethodInformation {
+	private final String methodName;
+	private final int numberOfCalls;
+	private final ICompilationUnit cu;
+	private final IMethod method;
+
+	public MethodInformation(String methodName, int numberOfCalls,
+			ICompilationUnit cu, IMethod method) {
+		this.methodName = methodName;
+		this.numberOfCalls = numberOfCalls;
+		this.cu = cu;
+		this.method = method;
+	}
+
+	/**
+	 * @return the method
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
+
+	/**
+	 * @return the numberOfCalls
+	 */
+	public int getNumberOfCalls() {
+		return numberOfCalls;
+	}
+
+	/**
+	 * @return the cu
+	 */
+	public ICompilationUnit getResource() {
+		return cu;
+	}
+
+	/**
+	 * @return the method
+	 */
+	public IMethod getMethod() {
+		return method;
+	}
+
+}
